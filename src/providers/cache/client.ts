8159082e7aca307e8f4fe6f230b551dc59c658e8
Gabriel Muller <ernesto.gabriel90@gmail.com>
2022-09-13 10:59:50 -0300

    fix(caching): defined contents data type

--- src/providers/cache/client.ts
+++ src/providers/cache/client.ts
@@ -1,11 +1,13 @@
 import { IClientCacheItem, defaultEmptyItemStringify } from './client.cache.types';
+import { IPodcastList } from '../podcasts/podcasts.type';
+import { IEpisodesList } from '../episodes/episodes.type';
 import dayjs from 'dayjs';
 
 export function getItem (keyMapper: string): IClientCacheItem  {
   return JSON.parse(localStorage.getItem(keyMapper) ?? defaultEmptyItemStringify);
 }
 
-export function saveItem(keyMapper: string, contents: unknown): IClientCacheItem {
+export function saveItem(keyMapper: string, contents: IPodcastList | IEpisodesList): IClientCacheItem {
   try {
     const alreadyItem = JSON.parse(localStorage.getItem(keyMapper) ?? defaultEmptyItemStringify);
     if (alreadyItem.contents.length === 0) {
