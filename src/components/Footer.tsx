e1a86f5a0befa6ad3b133dfc6b32aa9044311d4e
Gabriel Muller <ernesto.gabriel90@gmail.com>
2022-09-16 00:37:06 -0300

    refactor(podcaster): change favicon.ico

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,13 @@ export default function Footer() {
     <Box px='1rem' h='80px' bg='blue.300'>
       <Flex align='flex-end' direction='column-reverse'>
         <Text color='white'>Podcaster</Text>
+        <a 
+          href="https://www.flaticon.com/free-icons/podcast" 
+          title="podcast icons"
+          style={{display: 'none'}}
+        >
+          Podcast icons created by Smashicons - Flaticon
+        </a>
       </Flex>
     </Box>
   );
