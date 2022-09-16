4c783469255b60f210fe93c1b6bcd3744e1f2b4e
Gabriel Muller <ernesto.gabriel90@gmail.com>
2022-09-16 05:00:50 -0300

    fix(utils): remove unused function

--- src/providers/podcasts/fetchPodcast.ts
+++ src/providers/podcasts/fetchPodcast.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import endpoints from '../endpoints';
-import { destructringPodcastResponse } from './utils';
+import { destructringPodcastResponse } from 'utils';
 import { IPodcastList } from './podcasts.type';
 
 export default async function fetchPodcasts(): Promise<IPodcastList> {
